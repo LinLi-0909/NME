12cee578dceef8f1e20d024cbb5babeb0e168dcf
LinLi-0909 <63344240+LinLi-0909@users.noreply.github.com>
2022-12-26 10:37:52 +0800

    Delete calc_cNME.m

--- calc_cNME.m
+++
@@ -1,6 +0,0 @@
-input_data = importdata(filename);
-var_names = strip(input_data.textdata,'"');
-input_data = input_data.data;
-out_mat = Get_cDMI(input_data,var_names,var_names,nz,p,k);
-out = array2table(out_mat,'VariableNames',var_names,'RowNames',var_names);
-writetable(out,[output,'.csv'],'WriteRowNames',true);
